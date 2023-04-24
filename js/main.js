badadb2f5de495eb2fbb907200e80583c252d8d6
Wnika <wnikaherkt@gmail.com>
2023-04-24 21:53:11 +0200

    header

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 const footerYear = document.querySelector('.footer__year')
+const navMobile = document.querySelector('.nav-mobile')
+const navBtn = document.querySelector('.hamburger')
 
+const handleNav = () => {
+	navBtn.classList.toggle('is-active')
+	navMobile.classList.toggle('nav-mobile--active')
+}
+
+navBtn.addEventListener('click', handleNav)
 
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
@@ -7,5 +15,3 @@ const handleCurrentYear = () => {
 }
 
 handleCurrentYear()
-navBtn.addEventListener('click', handleNav)
-window.addEventListener('scroll', handleObserver)
