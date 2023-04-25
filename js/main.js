2abdddda604fa28102f5fcd59805ffe2dca9a381
Wnika <wnikaherkt@gmail.com>
2023-04-25 20:56:59 +0200

    cards added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 const footerYear = document.querySelector('.footer__year')
 const navMobile = document.querySelector('.nav-mobile')
 const navBtn = document.querySelector('.hamburger')
+const allNavLinks = document.querySelectorAll('.nav__link')
 
 const handleNav = () => {
 	navBtn.classList.toggle('is-active')
 	navMobile.classList.toggle('nav-mobile--active')
+
+	allNavLinks.forEach(item => {
+		item.addEventListener('click', () => {
+			navMobile.classList.remove('nav-mobile--active')
+		})
+	})
 }
 
 navBtn.addEventListener('click', handleNav)
